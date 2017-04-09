e42145a4c193f91f8ae0435df28fa94a5b0ec39c
zyssky <2462084760@qq.com>
2017-04-09 11:10:50 +0800

    init the project framework

---
+++ app/src/main/java/com/example/administrator/omg/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.administrator.omg;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity  {
+
+    private Context context;
+
+    private BottomNavigationView navigationItemView;
+
+    private ArrayList<Fragment> fragments;
+
+    private Fragment curFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        context = this;
+        initFragments();
+        initNavigationView();
+
+
+    }
+
+    void initFragments(){
+        fragments = new ArrayList<>();
+        fragments.add(HomeFragment.newInstance());
+        fragments.add(OrderFragment.newInstance());
+        fragments.add(AccountFragment.newInstance());
+
+        curFragment = fragments.get(0);
+        getSupportFragmentManager().beginTransaction().add(R.id.container,curFragment).commit();
+    }
+
+    void initNavigationView(){
+        navigationItemView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+
+        navigationItemView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        switchContent(curFragment,fragments.get(0));
+                        break;
+                    case R.id.nav_orders:
+                        switchContent(curFragment,fragments.get(1));
+                        break;
+                    case R.id.nav_account:
+                        switchContent(curFragment,fragments.get(2));
+                        break;
+                    default:
+                        return true;
+                }
+                return true;
+            }
+        });
+    }
+
+    public void switchContent(Fragment from, Fragment to) {
+        if (curFragment != to) {
+            curFragment = to;
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction().setCustomAnimations(
+                    android.R.anim.fade_in, android.R.anim.fade_out);
+            if (!to.isAdded()) {	// 先判断是否被add过
+                transaction.hide(from).add(R.id.container, to).commit(); // 隐藏当前的fragment，add下一个到Activity中
+            } else {
+                transaction.hide(from).show(to).commit(); // 隐藏当前的fragment，显示下一个
+            }
+        }
+    }
+}
